9324c2a5f20832680c37ca90be60bd0922dc6560
Dominik Guzei <dominik.guzei@gmail.com>
2013-11-22 12:28:30 +0100

    initializes project as grunt plugin

---
+++ Gruntfile.js
@@ -0,0 +1,56 @@
+/*
+ * grunt-xsl-transform
+ * https://github.com/dominik/grunt--xsl-transform
+ *
+ * Copyright (c) 2013 Dominik Guzei
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+
+    // Before generating any new files, remove any previously-created files.
+    clean: {
+      tests: ['tmp'],
+    },
+
+    // Configuration to be run (and then tested).
+    xsl_transform: {
+      default_options: {
+        options: {
+        },
+        files: {
+          'tmp/default_options': ['test/fixtures/testing', 'test/fixtures/123'],
+        },
+      },
+      custom_options: {
+        options: {
+          separator: ': ',
+          punctuation: ' !!!',
+        },
+        files: {
+          'tmp/custom_options': ['test/fixtures/testing', 'test/fixtures/123'],
+        },
+      },
+    },
+
+  });
+
+  // Actually load this plugin's task(s).
+  grunt.loadTasks('tasks');
+
+  // These plugins provide necessary tasks.
+  grunt.loadNpmTasks('grunt-contrib-clean');
+
+  // Whenever the "test" task is run, first clean the "tmp" dir, then run this
+  // plugin's task(s), then test the result.
+  grunt.registerTask('test', ['clean', 'xsl_transform']);
+
+  // By default, lint and run all tests.
+  grunt.registerTask('default', ['jshint']);
+
+};
